a22ec6492a31f3709a5345d1592e4d4518f34c84
ant <nivaniucc@gmail.com>
2021-05-22 19:09:16 -0400

    Trying to figure out this undefined that I'm currently getting

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { db } = require('./db/db');
+
+const { notes } = require("./db/db");
+console.log(notes);
+
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
-    });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, './public/index.html'));
-    });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.use(express.static('public'))
-  
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  notes.push(req.body);
+  res.json(req.body);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  for (var i = 0; i < notes.length; i++) {
+    if (id === notes[i].id) {
+      notes.splice(i, 1);
+    }
+  }
+  res.json({});
+});
+
 app.listen(PORT, () => {
-    console.log(`server now on port ${PORT}!`);
-  });
+  console.log(`server now on port ${PORT}!`);
+});
