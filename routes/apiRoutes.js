30e3c19a802000afe23853f18c4cce6ba977cbef
ant <nivaniucc@gmail.com>
2021-05-22 20:06:22 -0400

    Successfully added note deletion

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,24 @@
-// Havent gotten an error from not doing this yet, but this should get done next 
+const { notes } = require("../../db/db");
+const router = require("express").Router();
+
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  notes.push(req.body);
+  res.json(req.body);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  for (var i = 0; i < notes.length; i++) {
+    if (id === notes[i].id) {
+      notes.splice(i, 1);
+    }
+  }
+  res.json({});
+});
+
+module.exports = router;
